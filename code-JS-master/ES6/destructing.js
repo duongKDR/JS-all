b5bcd3eaba623eb3e35dae84c9e734b2a9615e20
RD <duongyang25@gmail.com>
2023-02-07 08:46:13 +0000

    first commit

---
+++ code-JS-master/ES6/destructing.js
@@ -0,0 +1,15 @@
+const person = {
+    nameF: 'D',
+    age: 18,
+    gender: 'female'    
+}
+// thông thường
+// let name = person.name;
+// let age = person.age;
+// let gender = person.gender;
+// es6
+let { name, age, gender } = person;
+console.log(nameF);
+console.log(age); 
+console.log(gender); 
+
